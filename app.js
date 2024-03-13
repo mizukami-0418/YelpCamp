ae592b3aa4a96a3de0b1bddc7ffdd76446b6211c
mzkm2843 <ff10mm11yy23tamk@gmail.com>
2024-03-13 20:59:43 +0900

    joiをインストール

--- app.js
+++ app.js
@@ -1,71 +1,109 @@
-const express = require('express');
-const path = require('path');
-const mongoose = require('mongoose');
-const ejsMate = require('ejs-mate');
-const methodOverride = require('method-override');
-const Campground = require('./models/campground');
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const ejsMate = require("ejs-mate");
+const catchAsync = require("./utils/catchAsync");
+const ExpressError = require("./utils/ExpressError");
+const methodOverride = require("method-override");
+const Campground = require("./models/campground");
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp',
-{ useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-        console.log('コネクションok!!!');
-    })
-    .catch(err => {
-        console.log('コネクションエラー!!!!!');
-        console.log(err);
-    });
+mongoose
+  .connect("mongodb://localhost:27017/yelp-camp", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("コネクションok!!!");
+  })
+  .catch((err) => {
+    console.log("コネクションエラー!!!!!");
+    console.log(err);
+  });
 
 const app = express();
 
-app.engine('ejs', ejsMate);
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.engine("ejs", ejsMate);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 // ミドルウェアの設定
-app.use(express.urlencoded({extended: true}));
-app.use(methodOverride('_method'));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
-app.get('/', (req, res) => {
-    res.render('home');
+app.get("/", (req, res) => {
+  res.render("home");
 });
 
-app.get('/campgrounds', async (req, res) => {
+app.get(
+  "/campgrounds",
+  catchAsync(async (req, res) => {
     const campgrounds = await Campground.find({});
-    res.render('campgrounds/index', { campgrounds });
+    res.render("campgrounds/index", { campgrounds });
+  })
+);
+
+app.get("/campgrounds/new", (req, res) => {
+  res.render("campgrounds/new");
 });
 
-app.get('/campgrounds/new', (req, res) => {
-    res.render('campgrounds/new');
-})
-
-app.get('/campgrounds/:id', async (req, res) => {
+app.get(
+  "/campgrounds/:id",
+  catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
-    res.render('campgrounds/show', { campground });
-});
+    res.render("campgrounds/show", { campground });
+  })
+);
 
-app.post('/campgrounds', async (req, res) => {
+app.post(
+  "/campgrounds",
+  catchAsync(async (req, res) => {
+    if (!req.body.campground) throw new ExpressError("不正なデータです", 400);
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
-});
+  })
+);
 
-app.get('/campgrounds/:id/edit', async (req, res) => {
+app.get(
+  "/campgrounds/:id/edit",
+  catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
-    res.render('campgrounds/edit', { campground });
-})
+    res.render("campgrounds/edit", { campground });
+  })
+);
 
-app.put('/campgrounds/:id', async (req, res) => {
+app.put(
+  "/campgrounds/:id",
+  catchAsync(async (req, res) => {
     const { id } = req.params;
-    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+    const campground = await Campground.findByIdAndUpdate(id, {
+      ...req.body.campground,
+    });
     res.redirect(`/campgrounds/${campground._id}`);
-});
+  })
+);
 
-app.delete('/campgrounds/:id', async (req, res) => {
+app.delete(
+  "/campgrounds/:id",
+  catchAsync(async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
-    res.redirect('/campgrounds');
-})
+    res.redirect("/campgrounds");
+  })
+);
+
+app.all("*", (req, res, next) => {
+  next(new ExpressError("ページが見つかりませんでした", 404));
+});
+
+app.use((err, req, res, next) => {
+  const { statusCode = 500 } = err;
+  if (!err.message) {
+    err.message = "問題が起きました";
+  }
+  res.status(statusCode).render("error", { err });
+});
 
 app.listen(3000, () => {
-    console.log('ポート3000でリクエスト待受中');
+  console.log("ポート3000でリクエスト待受中");
 });
