f8e2e0d424dbeee48ef7bef751451a0ae6c8e7f3
mzkm2843 <ff10mm11yy23tamk@gmail.com>
2024-03-16 21:50:35 +0900

    成功とエラーのflashメッセージを作成

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -33,6 +33,10 @@ router.get(
     const campground = await Campground.findById(req.params.id).populate(
       "reviews"
     );
+    if (!campground) {
+      req.flash("error", "キャンプ場が見つかりませんでした");
+      return res.redirect("/campgrounds");
+    }
     res.render("campgrounds/show", { campground });
   })
 );
@@ -43,6 +47,7 @@ router.post(
   catchAsync(async (req, res) => {
     const campground = new Campground(req.body.campground);
     await campground.save();
+    req.flash("success", "新しいキャンプ場を登録しました！！");
     res.redirect(`/campgrounds/${campground._id}`);
   })
 );
@@ -51,6 +56,10 @@ router.get(
   "/:id/edit",
   catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
+    if (!campground) {
+      req.flash("error", "キャンプ場が見つかりませんでした");
+      return res.redirect("/campgrounds");
+    }
     res.render("campgrounds/edit", { campground });
   })
 );
@@ -63,6 +72,7 @@ router.put(
     const campground = await Campground.findByIdAndUpdate(id, {
       ...req.body.campground,
     });
+    req.flash("success", `キャンプ場「${campground.title}」を更新しました`);
     res.redirect(`/campgrounds/${campground._id}`);
   })
 );
@@ -72,6 +82,7 @@ router.delete(
   catchAsync(async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
+    req.flash("success", "キャンプ場を削除しました");
     res.redirect("/campgrounds");
   })
 );
--- routes/reviews.js
+++ routes/reviews.js
@@ -25,6 +25,7 @@ router.post(
     campground.reviews.push(review);
     await review.save();
     await campground.save();
+    req.flash("success", "レビューを投稿しました");
     res.redirect(`/campgrounds/${campground._id}`);
   })
 );
@@ -34,6 +35,7 @@ router.delete(
     const { id, reviewId } = req.params;
     await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
     await Review.findByIdAndDelete(reviewId);
+    req.flash("success", "レビューを削除しました");
     res.redirect(`/campgrounds/${id}`);
   })
 );
