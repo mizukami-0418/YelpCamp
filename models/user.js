5d52b5ef9a73a6df663412d6059b823fd3de0ff0
mzkm2843 <ff10mm11yy23tamk@gmail.com>
2024-03-20 17:54:16 +0900

    passport関係のインストールとregister.ejsの作成

---
+++ models/user.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+const passportLocalMongoose = require("passport-local-mongoose");
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    require: true,
+    unique: true,
+  },
+});
+
+userSchema.plugin(passportLocalMongoose, {
+  errorMessages: {
+    UserExistsError: "そのユーザー名は既に登録済です",
+    MissingPasswordError: "パスワードを入力してください",
+    AttemptTooSoonError:
+      "アカウントがロックされています。時間を空けて再度お試しください",
+    TooManyAttemptsError: "アカウントをロックしました",
+    NoSaltValueStoredError: "認証ができませんでした",
+    IncorrectPasswordError: "ユーザー名またはパスワードが間違っています",
+    IncorrectUseError: "ユーザー名またはパスワードが間違っています",
+    MissingUsernameError: "そのユーザーは存在しません",
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
